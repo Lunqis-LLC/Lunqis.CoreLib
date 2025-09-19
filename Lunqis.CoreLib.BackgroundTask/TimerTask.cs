7a58673139ac157f8e7cad9f67d2cef88ceb1a09
牛奶 <burpee_soggier0x@icloud.com>
2025-09-19 14:33:35 +0900

    改善TimerTask的实现

--- Lunqis.CoreLib.BackgroundTask/TimerTask.cs
+++ Lunqis.CoreLib.BackgroundTask/TimerTask.cs
@@ -20,6 +20,7 @@
 //OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 //SOFTWARE.
 using System;
+using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -28,38 +29,49 @@ namespace Lunqis.CoreLib.BackgroundTask
     /// <summary>
     /// 按定时间隔执行的任务
     /// </summary>
-    public abstract class TimerTask : BackGroundTask
+    public abstract class TimerTask : ITask
     {
+        #region 静态执行部分
+        static TimerTask()
+        {
+            ThreadPool.QueueUserWorkItem(async (obj) => await StaticExecute((CancellationTokenSource)obj), TokenSource);
+        }
+        private static readonly List<TimerTask> Tasks = new List<TimerTask>();
+        private static async Task StaticExecute(CancellationTokenSource cancellationTokenSource)
+        {
+            if (cancellationTokenSource.IsCancellationRequested) return;
+            // 如果任务没有取消，则始终执行
+            while (!cancellationTokenSource.IsCancellationRequested)
+            {
+                foreach (var task in Tasks)
+                {
+                    if (!task.Enabled)
+                    {
+                        continue;
+                    }
+                    if (task.ExecuteDatetime <= DateTime.Now)
+                    {
+                        task.ExecuteDatetime = DateTime.Now.Add(task.Interval);
+                        await task.IntervalExecuteAsync(null, cancellationTokenSource.Token);
+                    }
+                }
+                await Task.Delay(TimeSpan.FromSeconds(0.1));
+            }
+        }
+        private static CancellationTokenSource TokenSource = new CancellationTokenSource();
+        #endregion
+
         /// <summary>
         /// 任务执行的时间
         /// </summary>
-        private DateTime ExecuteDatetime { get; set; }
+        private DateTime ExecuteDatetime { get; set; } = DateTime.MinValue;
 
         /// <summary>
         /// 任务执行的间隔
         /// </summary>
         protected TimeSpan Interval { get; set; }
 
-        /// <summary>
-        /// 开始执行后台任务
-        /// </summary>
-        /// <param name="input">传入参数</param>
-        /// <param name="token">Token</param>
-        /// <returns>异步任务</returns>
-        protected override async Task BackGroundExecuteAsync(object? input, CancellationToken token)
-        {
-            // 如果任务没有取消，则始终执行
-            while (!token.IsCancellationRequested)
-                // 如果当前时间大于等于执行时间，则执行任务
-                if (DateTime.Now >= ExecuteDatetime)
-                {
-                    // 计算下一次执行的时间
-                    ExecuteDatetime = DateTime.Now.Add(Interval);
-                    await IntervalExecuteAsync(input, token);
-                }
-                else
-                    await Task.Delay(Interval, token);
-        }
+        protected bool Enabled { get; set; }
 
         /// <summary>
         /// 执行定期间隔的任务
@@ -68,5 +80,28 @@ namespace Lunqis.CoreLib.BackgroundTask
         /// <param name="token">Token</param>
         /// <returns>异步任务</returns>
         protected abstract Task IntervalExecuteAsync(object? input, CancellationToken token);
+
+        /// <summary>
+        /// 开始执行后台任务
+        /// </summary>
+        /// <param name="input">传入参数</param>
+        /// <param name="token">Token</param>
+        /// <returns>异步任务</returns>
+        public async Task ExecuteAsync(object? input, CancellationToken token)
+        {
+            if (token.IsCancellationRequested) return;
+            if (Enabled) return;
+
+            token.Register(() =>
+            {
+                TokenSource.Cancel();
+                Enabled = false;
+            });
+
+            Enabled = true;
+            Tasks.Add(this);
+
+            await Task.CompletedTask;
+        }
     }
 }
